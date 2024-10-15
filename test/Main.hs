9c13dc1be1e1247f8cc7da3cda956858f004e173
Rodrigo Ribeiro <rodrigogribeiro@gmail.com>
2024-10-15 09:53:26 -0300

    Fixes on tests

--- test/Main.hs
+++ test/Main.hs
@@ -22,7 +22,7 @@ spec
               [
                 runTestForFile "00answer.solc" specFolder
               , runTestForFile "01id.solc" specFolder
-              , expectFail $ runTestForFile "02nid.solc" specFolder
+              , runTestForFile "02nid.solc" specFolder
               , runTestForFile "031maybe.solc" specFolder
               , runTestForFile "032simplejoin.solc" specFolder
               , runTestForFile "033join.solc" specFolder
@@ -34,7 +34,7 @@ spec
               , runTestForFile "039food.solc" specFolder
               , runTestForFile "041pair.solc" specFolder
               , runTestForFile "042triple.solc" specFolder
-              , expectFail $ runTestForFile "06comp.solc" specFolder
+              , runTestForFile "06comp.solc" specFolder
               , runTestForFile "07rgb.solc" specFolder
               , runTestForFile "08rgb2.solc" specFolder 
               , runTestForFile "09not.solc" specFolder 
@@ -64,13 +64,13 @@ cases
                 runTestForFile "Ackermann.solc" caseFolder 
               , expectFail $ runTestForFile "BadInstance.solc" caseFolder
               , runTestForFile "BoolNot.solc" caseFolder
-              , expectFail $ runTestForFile "Compose.solc" caseFolder
+              , runTestForFile "Compose.solc" caseFolder
               , expectFail $ runTestForFile "DupFun.solc" caseFolder
               , runTestForFile "DuplicateFun.solc" caseFolder
               , runTestForFile "EitherModule.solc" caseFolder
               , runTestForFile "Id.solc" caseFolder
               , runTestForFile "IncompleteInstDef.solc" caseFolder 
-              , runTestForFile "Invokable.solc" caseFolder
+              , expectFail $ runTestForFile "Invokable.solc" caseFolder
               , runTestForFile "ListModule.solc" caseFolder
               , runTestForFile "Logic.solc" caseFolder
               , runTestForFile "Memory1.solc" caseFolder
@@ -86,7 +86,7 @@ cases
               , runTestForFile "RefDeref.solc" caseFolder
               , expectFail $ runTestForFile "SillyReturn.solc" caseFolder
               , runTestForFile "SimpleField.solc" caseFolder
-              , runTestForFile "SimpleInvoke.solc" caseFolder
+              , expectFail $ runTestForFile "SimpleInvoke.solc" caseFolder
               , runTestForFile "SimpleLambda.solc" caseFolder
               , runTestForFile "SingleFun.solc" caseFolder
               , runTestForFile "assembly.solc" caseFolder
