f71759f90bcc96ea293f293b981a724f8045a7c8
Marcin Benke <marcin@benke.org>
2024-10-25 12:53:02 +0200

    remove poison builtin

--- yule/Builtins.hs
+++ yule/Builtins.hs
@@ -4,13 +4,15 @@ import Data.String
 import Language.Yul
 
 yulBuiltins :: Yul
-yulBuiltins = Yul poisonBuiltin
+yulBuiltins = Yul []
 
 revertStmt :: String -> [YulStmt]
 revertStmt s =  [ YExp $ YCall "mstore" [yulInt 0, YLit (YulString s)]
   , YExp $ YCall "revert" [yulInt 0, yulInt (length s)]
   ]
 
+{-
 poisonBuiltin :: [YulStmt]
 poisonBuiltin = 
     [ YFun "$poison" [] (YReturns ["_dummy"]) (revertStmt "Dying from poison!") ]
+-}
