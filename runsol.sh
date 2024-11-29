9dc777d3a62043c37eba77f264bd54cdb2683c74
rodrigogribeiro <rodrigogribeiro@gmail.com>
2024-11-29 09:24:35 -0300

    Fixes before merge

--- runsol.sh
+++ runsol.sh
@@ -1,6 +1,6 @@
 file=$1
 shift
 echo $file
-cabal run sol-core -- -f $file $* && \
-cabal run yule -- output.core && \
-forge script Output.sol
+cabal run sol-core -- -f $file $* &&
+  cabal run yule -- output.core &&
+  forge script Output.sol
