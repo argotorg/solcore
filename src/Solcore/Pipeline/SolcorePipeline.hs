cc5d17259fb168200c5327513a726ffdb15f2628
rodrigogribeiro <rodrigogribeiro@gmail.com>
2025-01-28 20:23:26 -0300

    Re-enabling specialization and pattern matching compilation

--- src/Solcore/Pipeline/SolcorePipeline.hs
+++ src/Solcore/Pipeline/SolcorePipeline.hs
@@ -60,20 +60,20 @@ pipeline = do
           mapM_ putStrLn (reverse $ logsInfo)
           putStrLn "> Elaborated tree:"
           putStrLn $ pretty c'
-        -- r8 <- matchCompiler c'
-        -- withErr r8 $ \ res -> do
-        --   when (verbose || optDumpDS opts) do
-        --     putStrLn "> Match compilation result:"
-        --     putStrLn (pretty res)
-        --   unless (optNoSpec opts) do
-        --     r9 <- specialiseCompUnit res (optDebugSpec opts) env
-        --     when (optDumpSpec opts) do
-        --       putStrLn "> Specialised contract:"
-        --       putStrLn (pretty r9)
-        --     r10 <- emitCore (optDebugCore opts) env r9
-        --     when (optDumpCore opts) do
-        --       putStrLn "> Core contract(s):"
-        --       forM_ r10 (putStrLn . pretty)
+        r8 <- matchCompiler c'
+        withErr r8 $ \ res -> do
+          when (verbose || optDumpDS opts) do
+            putStrLn "> Match compilation result:"
+            putStrLn (pretty res)
+          unless (optNoSpec opts) do
+            r9 <- specialiseCompUnit res (optDebugSpec opts) env
+            when (optDumpSpec opts) do
+              putStrLn "> Specialised contract:"
+              putStrLn (pretty r9)
+            r10 <- emitCore (optDebugCore opts) env r9
+            when (optDumpCore opts) do
+              putStrLn "> Core contract(s):"
+              forM_ r10 (putStrLn . pretty)
 
 runParser :: String -> IO (Either String (CompUnit Name))
 runParser content = do 
