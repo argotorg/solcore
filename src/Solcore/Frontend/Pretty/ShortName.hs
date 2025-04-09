1cfa3aa1649501280f0f609c6e2d73d27a5c95e4
Marcin Benke <marcin@benke.org>
2025-04-09 12:57:01 +0200

    add missing file

---
+++ src/Solcore/Frontend/Pretty/ShortName.hs
@@ -0,0 +1,40 @@
+module Solcore.Frontend.Pretty.ShortName where
+import Solcore.Frontend.Syntax.Contract
+import Solcore.Frontend.Syntax.Name 
+import Solcore.Frontend.Pretty.Name
+import Solcore.Frontend.Syntax.Stmt 
+import Solcore.Frontend.Syntax.Ty
+import Solcore.Frontend.TypeInference.Id
+import Solcore.Frontend.Pretty.SolcorePretty(pretty)
+
+import Common.Pretty
+prettys :: Pretty a => [a] -> String
+prettys = render . brackets . commaSep . map ppr
+
+class Pretty a => HasShortName a where
+  shortName :: a -> String
+  shortName = pretty 
+
+instance HasShortName Name
+instance HasShortName a => HasShortName (Contract a) where
+    shortName (Contract n _ _) = shortName n
+
+instance HasShortName a => HasShortName (Signature a) where
+    shortName sig = shortName (sigName sig) 
+
+instance HasShortName a => HasShortName (FunDef a) where
+    shortName fd = "function " ++shortName (funSignature fd)
+
+instance HasShortName a => HasShortName (TopDecl a) where
+  shortName (TContr c) = shortName c
+  shortName (TFunDef fd) = shortName fd
+  shortName (TClassDef c) = shortName (className c)
+  shortName (TInstDef is) = unwords 
+    [ pretty (mainTy is)
+    , ":"
+    , pretty (instName is)
+    , prettys (paramsTy is)
+    ]
+  shortName (TMutualDef ts) = concatMap shortName ts
+  shortName (TDataDef d) = pretty (dataName d)
+  shortName (TPragmaDecl p) = pretty p
