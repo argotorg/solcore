7cfc34ecaac9397fb06bbdea3ddd15c5be9f944b
rodrigogribeiro <rodrigogribeiro@gmail.com>
2025-04-21 14:43:41 -0300

    adds tests for simple imports

--- test/Cases.hs
+++ test/Cases.hs
@@ -57,7 +57,17 @@ pragmas
               ] 
     where 
       pragmaFolder = "./test/examples/pragmas"
- 
+
+imports :: TestTree 
+imports 
+  = testGroup "Files for imports cases"
+              [
+                runTestForFile "booldef.solc" importsFolder 
+              , runTestForFile "boolmain.solc" importsFolder
+              ]
+      where 
+        importsFolder = "./test/imports"
+
 cases :: TestTree 
 cases 
   = testGroup "Files for folder cases"
