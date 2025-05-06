610b463f3ccde61a611bdb62ec6c99fc6f1525c4
rodrigogribeiro <rodrigogribeiro@gmail.com>
2025-05-06 13:54:08 -0300

    Fixes on default instance selection.

--- src/Solcore/Frontend/TypeInference/TcReduce.hs
+++ src/Solcore/Frontend/TypeInference/TcReduce.hs
@@ -1,15 +1,15 @@
-module Solcore.Frontend.TypeInference.TcReduce where 
+module Solcore.Frontend.TypeInference.TcReduce where
 
 import Control.Monad
 import Control.Monad.Except
 import Control.Monad.Trans
 import Data.List
+import Data.Map qualified as Map
 import Data.Maybe
-import qualified Data.Map as Map
 
-import Solcore.Frontend.Syntax
 import Solcore.Frontend.Parser.SolverInputParser
 import Solcore.Frontend.Pretty.SolcorePretty
+import Solcore.Frontend.Syntax
 import Solcore.Frontend.TypeInference.TcEnv
 import Solcore.Frontend.TypeInference.TcMonad
 import Solcore.Frontend.TypeInference.TcSubst
@@ -17,35 +17,36 @@ import Solcore.Frontend.TypeInference.TcUnify
 
 import Solcore.Pipeline.Options
 
-
 splitContext :: [Pred] -> [Tyvar] -> TcM ([Pred], [Pred])
-splitContext ps fs 
-  = do
-      info [">> Starting the reduction of:", pretty ps]
-      ps' <- reduce ps
-      let (ds, rs) = partition (all (`elem` fs) . fv) ps'
-      info [">> Defered constraints:", pretty ds]
-      info [">> Retained constraints:", pretty rs]
-      pure (ds, rs)
- 
+splitContext ps fs =
+  do
+    info [">> Starting the reduction of:", pretty ps]
+    ps' <- reduce ps
+    let (ds, rs) = partition (all (`elem` fs) . fv) ps'
+    info [">> Defered constraints:", pretty ds]
+    info [">> Retained constraints:", pretty rs]
+    pure (ds, rs)
 
--- main context reduction function 
+-- main context reduction function
 
 reduce :: [Pred] -> TcM [Pred]
-reduce ps0
-  = do 
-      n <- askMaxRecursionDepth
-      ps' <- reduceI n ps0
-      simplify ps'
+reduce ps0 =
+  do
+    n <- askMaxRecursionDepth
+    ps' <- reduceI n ps0
+    simplify ps'
 
 reduceI :: Int -> [Pred] -> TcM [Pred]
 reduceI n ps0
-  | n <= 0 = tcmError $ unwords [ "Cannot reduce"
-                                , pretty ps0
-                                , "since the solver exceeded the max number of iterations"
-                                ]
-  | otherwise 
-    = do
+  | n <= 0 =
+      tcmError $
+        unwords
+          [ "Cannot reduce"
+          , pretty ps0
+          , "since the solver exceeded the max number of iterations"
+          ]
+  | otherwise =
+      do
         ps <- withCurrentSubst ps0
         preds <- concat <$> mapM reduceBySuper ps
         unless (null preds) $ info ["> reduce context ", pretty preds]
@@ -53,165 +54,191 @@ reduceI n ps0
         unless (null ps1) $ info ["< reduced context ", pretty (nub ps1)]
         pure (nub ps1)
 
--- simplify by entailment 
+-- simplify by entailment
 
 simplify :: [Pred] -> TcM [Pred]
 simplify = loop []
-  where 
-    loop rs [] = pure rs 
-    loop rs (p : ps) = do 
-      c <- entails (rs ++ ps) p 
-      if c then loop rs ps 
-        else loop (p : rs) ps
+ where
+  loop rs [] = pure rs
+  loop rs (p : ps) = do
+    c <- entails (rs ++ ps) p
+    if c
+      then loop rs ps
+      else loop (p : rs) ps
 
 -- reducing by using instance information
 
 reduceByInst :: Int -> [Pred] -> TcM [Pred]
-reduceByInst n ps 
-  = (nub . concat) <$> mapM (reduceByInst' n) ps 
+reduceByInst n ps =
+  (nub . concat) <$> mapM (reduceByInst' n) ps
 
 reduceByInst' :: Int -> Pred -> TcM [Pred]
-reduceByInst' n p@(InCls c _ _) 
-  | n <= 0 
-    = tcmError $ unwords [ "Cannot reduce"
-                         , pretty p
-                         , "since the solver exceeded the max number of iterations."
-                         ]
+reduceByInst' n p@(InCls c _ _)
+  | n <= 0 =
+      tcmError $
+        unwords
+          [ "Cannot reduce"
+          , pretty p
+          , "since the solver exceeded the max number of iterations."
+          ]
   | inHnf p = pure [p]
-  | otherwise 
-    = do 
+  | otherwise =
+      do
         ce <- getInstEnv
         insts <- askInstEnv c
         case selectInst ce p of
           Nothing -> do
-            de <- getDefaultInstEnv 
-            case selectDefaultInst de p of
-              Nothing -> tcmError $ unwords [ "No instance found for:", pretty p]
-              Just (ps', s, _) -> do 
-                extSubst s 
-                withCurrentSubst ps'
-          Just (preds, subst', instd) -> do 
-            extSubst subst' 
+            de <- getDefaultInstEnv
+            if proveDefaulting ce p
+              then do
+                case selectDefaultInst de p of
+                  Nothing -> tcmError $ unwords ["No instance found for:", pretty p]
+                  Just (ps', s, _) -> do
+                    extSubst s
+                    withCurrentSubst ps'
+              else tcmError $ unwords ["No instance found for:", pretty p]
+          Just (preds, subst', instd) -> do
+            extSubst subst'
             ps' <- reduceByInst (n - 1) preds
             pure ps'
-reduceByInst' n (t1 :~: t2) 
-  = do 
-      unify t1 t2
-      pure []
+reduceByInst' n (t1 :~: t2) =
+  do
+    unify t1 t2
+    pure []
+
+proveDefaulting :: InstTable -> Pred -> Bool
+proveDefaulting ce p@(InCls i t as) =
+  isNothing (msum [tryInst it | it <- insts ce i])
+ where
+  insts m n = maybe [] id (Map.lookup n m)
+  tryInst :: Qual Pred -> Maybe Inst
+  tryInst i@(_ :=> h) =
+    case mguPred h p of
+      Left _ -> Nothing
+      Right _ -> Just i
 
 selectDefaultInst :: Table Inst -> Pred -> Maybe ([Pred], Subst, Inst)
-selectDefaultInst de p@(InCls i t as)
-  = case Map.lookup i de of 
-      Nothing -> Nothing 
-      Just c@(ps :=> (InCls _ t' ts')) -> 
-        case match t' t of 
-          Left _ -> Nothing 
-          Right u -> 
-            case unifyTypes as ts' of
-              Left _ -> Nothing 
-              Right u' -> 
-                let u1 = u' <> u 
-                in Just (map (apply u1) ps, u1, c)
-selectDefaultInst _ _ = Nothing 
+selectDefaultInst de p@(InCls i t as) =
+  case Map.lookup i de of
+    Nothing -> Nothing
+    Just c@(ps :=> (InCls _ t' ts')) ->
+      case match t' t of
+        Left _ -> Nothing
+        Right u ->
+          case unifyTypes as ts' of
+            Left _ -> Nothing
+            Right u' ->
+              let u1 = u' <> u
+               in Just (map (apply u1) ps, u1, c)
+selectDefaultInst _ _ = Nothing
 
 selectInst :: InstTable -> Pred -> Maybe ([Pred], Subst, Inst)
-selectInst ce p@(InCls i t as) 
-  = msum [tryInst it | it <- insts ce i] 
-    where
-      insts m n = maybe [] id (Map.lookup n m)
-      tryInst :: Qual Pred -> Maybe ([Pred], Subst, Inst)
-      tryInst c@(ps :=> (InCls _ t' ts)) =
-          case match t' t of
-            Left _ -> Nothing
-            Right u -> case unifyTypes as ts of 
-                          Left _ -> Nothing 
-                          Right u' -> 
-                            let
-                                u1 = u' <> u
-                            in Just (map (apply u1) ps, u1, c)
+selectInst ce p@(InCls i t as) =
+  msum [tryInst it | it <- insts ce i]
+ where
+  insts m n = maybe [] id (Map.lookup n m)
+  tryInst :: Qual Pred -> Maybe ([Pred], Subst, Inst)
+  tryInst c@(ps :=> (InCls _ t' ts)) =
+    case match t' t of
+      Left _ -> Nothing
+      Right u -> case unifyTypes as ts of
+        Left _ -> Nothing
+        Right u' ->
+          let
+            u1 = u' <> u
+           in
+            Just (map (apply u1) ps, u1, c)
 selectInst _ _ = Nothing
 
-
--- reducing by super class info 
+-- reducing by super class info
 
 reduceBySuper :: Pred -> TcM [Pred]
-reduceBySuper p 
-  = do 
-      n <- askMaxRecursionDepth
-      reduceBySuper' n p 
+reduceBySuper p =
+  do
+    n <- askMaxRecursionDepth
+    reduceBySuper' n p
 
 reduceBySuper' :: Int -> Pred -> TcM [Pred]
 reduceBySuper' _ (_ :~: _) = pure []
-reduceBySuper' n p@(InCls c _ _) 
-  | n <= 0 = tcmError $ unwords [ "Cannot reduce:"
-                                , pretty p  
-                                , "since the solver exceeded the max number of iterations."
-                                ]
-  | otherwise = do 
-      ctbl <- getClassEnv 
-      case Map.lookup c ctbl of 
+reduceBySuper' n p@(InCls c _ _)
+  | n <= 0 =
+      tcmError $
+        unwords
+          [ "Cannot reduce:"
+          , pretty p
+          , "since the solver exceeded the max number of iterations."
+          ]
+  | otherwise = do
+      ctbl <- getClassEnv
+      case Map.lookup c ctbl of
         Nothing -> pure [p]
         Just cinfo -> do
-           ps' <- concat <$> mapM (reduceBySuper' (n - 1)) (supers cinfo)
-           pure (p : ps')
+          ps' <- concat <$> mapM (reduceBySuper' (n - 1)) (supers cinfo)
+          pure (p : ps')
 
--- entailment 
+-- entailment
 
-entails :: [Pred] -> Pred -> TcM Bool 
-entails qs p 
-  = do 
-      n <- askMaxRecursionDepth
-      entails' n qs p 
+entails :: [Pred] -> Pred -> TcM Bool
+entails qs p =
+  do
+    n <- askMaxRecursionDepth
+    entails' n qs p
 
-entails' :: Int -> [Pred] -> Pred -> TcM Bool 
-entails' n qs p  
-  | n <= 0 = tcmError $ unwords [ "Cannot reduce:"
-                                , pretty p  
-                                , "since the solver exceeded the max number of iterations."
-                                ]
+entails' :: Int -> [Pred] -> Pred -> TcM Bool
+entails' n qs p
+  | n <= 0 =
+      tcmError $
+        unwords
+          [ "Cannot reduce:"
+          , pretty p
+          , "since the solver exceeded the max number of iterations."
+          ]
   | otherwise = do
       ce <- getInstEnv
       qs' <- mapM reduceBySuper qs
-      r <- case selectInst ce p of 
-             Nothing -> pure False 
-             Just (ps, s, h) -> 
-                and <$> mapM (entails' (n - 1) ps) (apply s ps)
-      pure $ (any (p `elem`) qs') || r 
+      r <- case selectInst ce p of
+        Nothing -> pure False
+        Just (ps, s, h) ->
+          and <$> mapM (entails' (n - 1) ps) (apply s ps)
+      pure $ (any (p `elem`) qs') || r
 
--- hnf for predicates 
+-- hnf for predicates
 
-inHnf :: Pred -> Bool 
-inHnf (InCls _ t _) = hnf t 
+inHnf :: Pred -> Bool
+inHnf (InCls _ t _) = hnf t
 inHnf _ = False
 
--- head normal form for types 
+-- head normal form for types
 
-hnf :: Ty -> Bool 
+hnf :: Ty -> Bool
 hnf (TyVar _) = True
 hnf _ = False
 
--- function matches 
--- We assume that we do not support 
+-- function matches
+-- We assume that we do not support
 -- overlapping instances.
 
 matches :: Pred -> TcM (Maybe (Subst, [Pred]))
-matches p@(InCls c t ts) 
-  = do 
-      insts <- askInstEnv c 
-      ms <- catMaybes <$> mapM (genM t) insts 
-      case ms of 
-        [] -> pure Nothing 
-        [x] -> pure (Just x) 
-        ins   -> throwError $ unwords [ "Constraint"
-                                      , pretty p
-                                      , "has more than one matching instance."
-                                      ]
+matches p@(InCls c t ts) =
+  do
+    insts <- askInstEnv c
+    ms <- catMaybes <$> mapM (genM t) insts
+    case ms of
+      [] -> pure Nothing
+      [x] -> pure (Just x)
+      ins ->
+        throwError $
+          unwords
+            [ "Constraint"
+            , pretty p
+            , "has more than one matching instance."
+            ]
 matches p = tcmError $ "Invalid constraint:" ++ pretty p
 
 genM :: Ty -> Inst -> TcM (Maybe (Subst, [Pred]))
-genM t k@(ps :=> h@(InCls _ t' _)) 
-  = do 
-      r <- defaultM (match t t')  
-      case r of 
-        Just s -> pure $ Just (s, apply s ps) 
-        Nothing -> pure Nothing
+genM t k@(ps :=> h@(InCls _ t' _)) =
+  do
+    r <- defaultM (match t t')
+    case r of
+      Just s -> pure $ Just (s, apply s ps)
+      Nothing -> pure Nothing
--- src/Solcore/Frontend/TypeInference/TcUnify.hs
+++ src/Solcore/Frontend/TypeInference/TcUnify.hs
@@ -1,147 +1,170 @@
 module Solcore.Frontend.TypeInference.TcUnify where
 
-import Control.Monad 
+import Control.Monad
 import Control.Monad.Except
-import Control.Monad.Reader 
+import Control.Monad.Reader
 
 import Data.List
 
+import Common.Pretty
+import Solcore.Frontend.Pretty.SolcorePretty hiding ((<>))
 import Solcore.Frontend.Syntax
 import Solcore.Frontend.TypeInference.TcSubst
-import Common.Pretty
-import Solcore.Frontend.Pretty.SolcorePretty  hiding((<>))
 
+-- standard unification machinery
 
--- standard unification machinery 
-
-varBind :: MonadError String m => Tyvar -> Ty -> m Subst 
+varBind :: (MonadError String m) => Tyvar -> Ty -> m Subst
 varBind v t
   | t == TyVar v = return mempty
-  | v `elem` fv t = infiniteTyErr v t 
-  | otherwise = do 
+  | v `elem` fv t = infiniteTyErr v t
+  | otherwise = do
       return (v +-> t)
 
-isTyCon :: Ty -> Bool 
-isTyCon (TyCon _ _) = True 
+isTyCon :: Ty -> Bool
+isTyCon (TyCon _ _) = True
 isTyCon _ = False
 
--- type matching 
+-- type matching
 
-class Match a where 
-  match :: MonadError String m => a -> a -> m Subst 
+class Match a where
+  match :: (MonadError String m) => a -> a -> m Subst
 
-instance Match Ty where 
-  match (TyCon n ts) (TyCon n' ts') 
-    | n == n' = match ts ts' 
-  match t1@(TyVar v) t 
-    | t1 == t = pure mempty 
-    | otherwise = do 
+instance Match Ty where
+  match (TyCon n ts) (TyCon n' ts')
+    | n == n' = match ts ts'
+  match t1@(TyVar v) t
+    | t1 == t = pure mempty
+    | otherwise = do
         pure (v +-> t)
   match t1 t2 = typesNotMatch t1 t2
 
-instance (Pretty a, Match a) => Match [a] where 
-  match [] [] = pure mempty 
-  match (t : ts) (t' : ts') 
-    = do 
-        s1 <- match t t'
-        s2 <- match ts ts' 
-        merge s1 s2 
+instance (Pretty a, Match a) => Match [a] where
+  match [] [] = pure mempty
+  match (t : ts) (t' : ts') =
+    do
+      s1 <- match t t'
+      s2 <- match ts ts'
+      merge s1 s2
   match ts ts' = typesMatchListErr (map pretty ts) (map pretty ts')
 
-instance Match Pred where 
-  match (InCls n t ts) (InCls n' t' ts') 
+instance Match Pred where
+  match (InCls n t ts) (InCls n' t' ts')
     | n == n' = match (t : ts) (t' : ts')
     | otherwise = throwError "Classes differ!"
 
-instance (HasType a, Match a) => Match (Qual a) where 
-  match (ps :=> t) (ps' :=> t') 
-    = do 
-        s1 <- match t t'
-        s2 <- match (apply s1 t) (apply s1 t')
-        merge s1 s2 
+instance (HasType a, Match a) => Match (Qual a) where
+  match (ps :=> t) (ps' :=> t') =
+    do
+      s1 <- match t t'
+      s2 <- match (apply s1 t) (apply s1 t')
+      merge s1 s2
 
--- most general unifier 
+-- most general unifier
 
-mgu :: MonadError String m => Ty -> Ty -> m Subst 
-mgu (TyCon n ts) (TyCon n' ts') 
-  | n == n' && length ts == length ts' 
-    = solve (zip ts ts') mempty 
-mgu (TyVar v) t = varBind v t 
-mgu t (TyVar v) = varBind v t 
-mgu t1 t2 = typesDoNotUnify t1 t2 
+mgu :: (MonadError String m) => Ty -> Ty -> m Subst
+mgu (TyCon n ts) (TyCon n' ts')
+  | n == n' && length ts == length ts' =
+      solve (zip ts ts') mempty
+mgu (TyVar v) t = varBind v t
+mgu t (TyVar v) = varBind v t
+mgu t1 t2 = typesDoNotUnify t1 t2
 
-solve :: MonadError String m => [(Ty,Ty)] -> Subst -> m Subst 
-solve [] s = pure s 
-solve ((t1, t2) : ts) s 
-  = do 
-      s1 <- mgu (apply s t1) (apply s t2)
-      s2 <- solve ts s1 
-      pure (s2 <> s1)
+mguPred :: (MonadError String m) => Pred -> Pred -> m Subst
+mguPred p@(InCls n t ts) p'@(InCls n' t' ts')
+  | n == n' = unifyTypes (t : ts) (t' : ts')
+  | otherwise =
+      throwError $
+        unlines
+          [ "Cannot unify predicates:"
+          , pretty p
+          , "with"
+          , pretty p'
+          ]
+mguPred (t1 :~: t2) (t1' :~: t2') =
+  unifyTypes [t1, t2] [t1', t2']
 
-unifyTypes :: MonadError String m => [Ty] -> [Ty] -> m Subst 
+solve :: (MonadError String m) => [(Ty, Ty)] -> Subst -> m Subst
+solve [] s = pure s
+solve ((t1, t2) : ts) s =
+  do
+    s1 <- mgu (apply s t1) (apply s t2)
+    s2 <- solve ts s1
+    pure (s2 <> s1)
+
+unifyTypes :: (MonadError String m) => [Ty] -> [Ty] -> m Subst
 unifyTypes ts ts' = solve (zip ts ts') mempty
 
-unifyAllTypes :: MonadError String m => [Ty] -> m Subst 
-unifyAllTypes [] = pure mempty 
-unifyAllTypes (t : ts) 
-  = do 
-      s1 <- unifyAllTypes ts 
-      s2 <- mgu (apply s1 t) (apply s1 t)
-      pure (s2 <> s1)
+unifyAllTypes :: (MonadError String m) => [Ty] -> m Subst
+unifyAllTypes [] = pure mempty
+unifyAllTypes (t : ts) =
+  do
+    s1 <- unifyAllTypes ts
+    s2 <- mgu (apply s1 t) (apply s1 t)
+    pure (s2 <> s1)
 
 -- composition operator for matching
 
-merge :: MonadError String m => Subst -> Subst -> m Subst
-merge s1@(Subst p1) s2@(Subst p2) = if agree then pure (Subst (p1 ++ p2))
-                                             else mergeError disagree 
-  where
-    disagree = foldr step [] (dom p1 `intersect` dom p2)
-    step v ac 
-      | alphaEq (apply s1 (TyVar v)) (apply s2 (TyVar v)) = ac 
-      | otherwise = (apply s1 (TyVar v), apply s2 (TyVar v)) : ac 
-    agree = all (\v -> alphaEq (apply s1 (TyVar v)) (apply s2 (TyVar v)))
-                (dom p1 `intersect` dom p2)
-    dom s = map fst s
+merge :: (MonadError String m) => Subst -> Subst -> m Subst
+merge s1@(Subst p1) s2@(Subst p2) =
+  if agree
+    then pure (Subst (p1 ++ p2))
+    else mergeError disagree
+ where
+  disagree = foldr step [] (dom p1 `intersect` dom p2)
+  step v ac
+    | alphaEq (apply s1 (TyVar v)) (apply s2 (TyVar v)) = ac
+    | otherwise = (apply s1 (TyVar v), apply s2 (TyVar v)) : ac
+  agree =
+    all
+      (\v -> alphaEq (apply s1 (TyVar v)) (apply s2 (TyVar v)))
+      (dom p1 `intersect` dom p2)
+  dom s = map fst s
 
-mergeError :: MonadError String m => [(Ty, Ty)] -> m a 
-mergeError ts = throwError $ unlines $ "Cannot match types:" : ss 
-  where 
-    ss = map go ts 
-    go (x,y) = pretty x ++ " with " ++ pretty y
+mergeError :: (MonadError String m) => [(Ty, Ty)] -> m a
+mergeError ts = throwError $ unlines $ "Cannot match types:" : ss
+ where
+  ss = map go ts
+  go (x, y) = pretty x ++ " with " ++ pretty y
 
--- basic error messages 
+-- basic error messages
 
-infiniteTyErr :: MonadError String m => Tyvar -> Ty -> m a 
-infiniteTyErr v t 
-  = throwError $ unwords ["Cannot construct the infinite type:"
-                         , pretty v 
-                         , "~"
-                         , pretty t
-                         ] 
+infiniteTyErr :: (MonadError String m) => Tyvar -> Ty -> m a
+infiniteTyErr v t =
+  throwError $
+    unwords
+      [ "Cannot construct the infinite type:"
+      , pretty v
+      , "~"
+      , pretty t
+      ]
 
-typesNotMatch :: MonadError String m => Ty -> Ty -> m a 
-typesNotMatch t1 t2 
-  = throwError $ unwords [ "Types do not match:"
-                         , pretty t1 
-                         , "and"
-                         , pretty t2
-                         ]
+typesNotMatch :: (MonadError String m) => Ty -> Ty -> m a
+typesNotMatch t1 t2 =
+  throwError $
+    unwords
+      [ "Types do not match:"
+      , pretty t1
+      , "and"
+      , pretty t2
+      ]
 
-typesMatchListErr :: MonadError String m => [String] -> [String] -> m a 
-typesMatchListErr ts ts' 
-  = throwError (errMsg (zip ts ts'))
-    where 
-      errMsg ps = unwords  ["Types do not match:"] ++ 
-                           concatMap tyList ps  
-      tyList (t1, t2) = t1 <> " and " <> t2
+typesMatchListErr :: (MonadError String m) => [String] -> [String] -> m a
+typesMatchListErr ts ts' =
+  throwError (errMsg (zip ts ts'))
+ where
+  errMsg ps =
+    unwords ["Types do not match:"]
+      ++ concatMap tyList ps
+  tyList (t1, t2) = t1 <> " and " <> t2
 
-typesDoNotUnify :: MonadError String m => Ty -> Ty -> m a 
-typesDoNotUnify t1 t2 
-  = throwError $ unwords [ "Types:"
-                         , pretty t1
-                         , "and"
-                         , pretty t2
-                         , "do not unify"
-                         ]
-
+typesDoNotUnify :: (MonadError String m) => Ty -> Ty -> m a
+typesDoNotUnify t1 t2 =
+  throwError $
+    unwords
+      [ "Types:"
+      , pretty t1
+      , "and"
+      , pretty t2
+      , "do not unify"
+      ]
 
