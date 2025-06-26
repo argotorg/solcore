2c2c9d5667ab817b2b25e929518c324558c4ee6a
dxo <hi@d-xo.org>
2025-06-26 16:51:53 +0200

    scc: add MutualDef blocks to connected components

--- src/Solcore/Frontend/TypeInference/SccAnalysis.hs
+++ src/Solcore/Frontend/TypeInference/SccAnalysis.hs
@@ -38,7 +38,7 @@ sccAnalysis' :: CompUnit Name -> SCC (CompUnit Name)
 sccAnalysis' (CompUnit imps ds)
   = do
       cs' <- mapM sccContract cs
-      CompUnit imps <$> analysis (cs' ++ ds')
+      CompUnit imps <$> analysis (\ds -> singleton (TMutualDef ds)) (cs' ++ ds')
     where
       isContract (TContr _) = True
       isContract _ = False
@@ -49,17 +49,19 @@ sccAnalysis' (CompUnit imps ds)
 
 sccContract :: TopDecl Name -> SCC (TopDecl Name)
 sccContract (TContr (Contract n vs ds))
-  = (TContr . Contract n vs) <$> analysis ds
+  = (TContr . Contract n vs) <$> analysis (\ds -> singleton (CMutualDecl ds)) ds
 sccContract d = pure d
 
-analysis :: (Ord a, Names a, Decl a) => [a] -> SCC [a]
-analysis ds
+analysis :: (Ord a, Names a, Decl a, Show a) => ([a] -> [a]) -> [a] -> SCC [a]
+analysis fn ds
   = do
       let grph = mkGraph ds
           cmps = scc grph
       case topSort cmps of
         Left _ -> pure []
-        Right ds' -> pure $ reverse $ concatMap (toList . N.vertexList1) ds'
+        Right ds' -> pure $ reverse $ concatMap (fn . toList . N.vertexList1) ds'
+
+-- building the dependency graph
 
 -- building the dependency graph
 
