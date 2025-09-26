11ce54c2cdb82a7fe754e7a845da4646ed691267
Marcin Benke <marcin@benke.org>
2025-09-26 11:23:45 +0200

    EmitCore: nested objects for constracts with a constructor

--- src/Solcore/Desugarer/EmitCore.hs
+++ src/Solcore/Desugarer/EmitCore.hs
@@ -106,7 +106,9 @@ type Translation a = EM (a, [Core.Stmt])
 type CoreName = String
 
 emitTopDecl :: TopDecl Id -> EM [Core.Object]
-emitTopDecl (TContr c) = fmap pure (emitContract c)
+emitTopDecl (TContr c) = withLocalState do
+    runtimeObj <- emitContract c
+    pure [runtimeObj]
 emitTopDecl (TDataDef dt) = addData dt >> pure []
 emitTopDecl _ = pure []
 
@@ -122,22 +124,37 @@ emitContract :: Contract Id -> EM Core.Object
 emitContract c = do
     let cname = show (name c)
     writes ["Emitting core for contract ", cname]
-    coreBody <- concatMapM emitCDecl (decls c)
+    runtimeBody <- concatMapM emitCDecl (decls c)
     deployer <- gets ecDeployer
     case deployer of
-      Nothing -> pure(Core.Object cname coreBody [])
-
+      Nothing -> pure(Core.Object cname runtimeBody [])
+      Just code -> let runtimeObject = Core.Object cname runtimeBody []
+        in pure(Core.Object (cname++"Deploy") code [runtimeObject] )
 emitCDecl :: ContractDecl Id -> EM [Core.Stmt]
 emitCDecl cd@(CFunDecl f) = do
     -- debug ["!! emitCDecl ", show cd]
     emitFunDef f
-emitCDecl (CMutualDecl ds) = do
-  body <- concatMapM emitCDecl ds
-  pure [Core.SBlock body]
+emitCDecl (CMutualDecl ds) = case findConstructor ds of
+  Nothing -> do
+    body <- concatMapM emitCDecl ds
+    pure [Core.SBlock body]
+  Just _ -> do -- this is the deployer block
+    depDecls <- concatMapM emitCDecl ds
+    modify (\s -> s { ecDeployer = Just depDecls})
+    pure [] -- deployer code gets emitted later
 emitCDecl cd@(CDataDecl dt) = do
     -- debug ["!! emitCDecl ", show cd]
     addData dt >> pure []
 emitCDecl cd = debug ["!! emitCDecl ", show cd] >> pure []
+
+-- By now, constructor should be converted to a function "constructor"
+
+findConstructor :: [ContractDecl Id] -> Maybe (FunDef Id)
+findConstructor = go where
+  go [] = Nothing
+  go (CFunDecl d:ds)| isConstructor d = Just d
+  go (_:ds) = go ds
+  isConstructor (FunDef sig _) = sigName sig == Name "constructor"
 
 -----------------------------------------------------------------------
 -- Translating function definitions
