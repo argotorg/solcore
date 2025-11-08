004094a6841d83db2a59ae51953a13eb3608883b
Marcin Benke <marcin@benke.org>
2025-11-08 15:45:07 +0100

    Constraint for Stmt extensions

--- src/Solcore/Frontend/Syntax/StmtX.hs
+++ src/Solcore/Frontend/Syntax/StmtX.hs
@@ -1,18 +1,42 @@
+{-# LANGUAGE GHC2021, DataKinds, GADTs, StandaloneDeriving, ConstraintKinds, UndecidableInstances #-}
 module Solcore.Frontend.Syntax.StmtX where
-
+import GHC.Types
 import Data.Generics (Data, Typeable)
 
 import Solcore.Frontend.Syntax.Ty
 import Solcore.Frontend.Syntax.Name
 import Language.Yul
 
+data Pass = Parsed | Named | Typechecked deriving (Eq, Ord, Show, Data, Typeable)
+data ComPass (c::Pass) where
+    ComNm :: ComNm
+    ComPs :: ComPs
+    ComTc :: ComTc
+
+type ComNm = ComPass 'Named
+type ComPs = ComPass 'Parsed
+type ComTc = ComPass 'Typechecked
+
+data NoExtField = NoExtField deriving (Eq, Ord, Show, Data, Typeable)
+instance Semigroup NoExtField where _ <> _ = NoExtField
+instance Monoid NoExtField where mempty = NoExtField
+
+data DataConCantHappen -- empty data type
+-- void :: DataConCantHappen
+-- void = error "Attempt to evaluate void"
+
+absurd :: DataConCantHappen -> a
+absurd v = case v of {}
+
 -- definition of statements
 
 type EquationX x = ([Pat x], [StmtX x])
 type EquationsX x = [EquationX x]
 
-type NamedExp = ExpX 'Named
-pattern (:=) :: NamedExp -> NamedExp -> NamedExp
+type NamedExp = ExpX ComNm
+type NamedStmt = StmtX ComNm
+
+pattern (:=) :: NamedExp -> NamedExp -> NamedStmt
 pattern (:=) e1 e2 <- AssX _ e1 e2
   where e1 := e2 = AssX mempty e1 e2
 
@@ -24,7 +48,21 @@ data StmtX x
   | MatchX (XMatch x) [ExpX x] (EquationsX x)         -- pattern matching
   | AsmX (XAsm x) YulBlock                        -- Yul block (type annotations ???)
   | IfX (XIf x)(ExpX x) (BodyX x) (BodyX x)        -- If statement
-  deriving (Eq, Ord, Show, Data, Typeable)
+  | XStmtX (XXStmt x)                             -- extensions
+
+type ForallStmtX (p:: Type -> Constraint) x =
+  ( p(XAss x)
+  , p(XLet x)
+  , p(XStmtExp x)
+  , p(XReturn x)
+  , p(XMatch x)
+  , p(XIf x)
+  , p(XXStmt x)
+  )
+-- deriving instance (Eq, Ord, Show, Data, Typeable)
+-- deriving instance Eq (StmtX x)
+-- deriving instance Ord (StmtX x)
+-- deriving instance Show (StmtX x)
 
 type family XAss x
 type family XLet x
@@ -33,16 +71,33 @@ type family XReturn x
 type family XMatch x
 type family XAsm x
 type family XIf x
+type family XXStmt x
+
+type instance XAss ComNm = NoExtField
+type instance XLet ComNm = NoExtField
+type instance XStmtExp ComNm = NoExtField
+type instance XReturn ComNm = NoExtField
+type instance XMatch ComNm = NoExtField
+type instance XAsm ComNm = NoExtField
+type instance XIf ComNm = NoExtField
+type instance XXStmt ComNm = NoExtField
+
 
 type BodyX x = [StmtX x]
 
 data ParamX x
   = TypedX (XTyped x) Name Ty
   | UntypedX (XUntyped x)  Name
-  deriving (Eq, Ord, Show, Data, Typeable)
+--  deriving (Eq, Ord, Show, Data, Typeable)
+
+type family XTyped x
+type family XUntyped x
+
+type instance XTyped ComNm = NoExtField
+type instance XUntyped ComNm = NoExtField
 
 
-paramName :: Param a -> a
+paramName :: ParamX x -> Name
 paramName (TypedX _ n _) = n
 paramName (UntypedX _ n) = n
 
@@ -54,10 +109,10 @@ data ExpX x
   | FieldAccessX (XFA x) (Maybe (ExpX x)) Name      -- field access
   | LitX (XLit x) Literal                        -- literal
   | CallX (XCall x) (Maybe (ExpX x)) Name [ExpX x]     -- function calal
-  | LamX (XLam x)[Param Name] (Body Name) (Maybe Ty)  -- lambda-abstraction
+  | LamX (XLam x)[ParamX x] (BodyX x) (Maybe Ty)  -- lambda-abstraction
   | TyExpX (XTyExp x)(ExpX x) Ty                   -- type annotated expression
   | CondX (XCond x) (ExpX x) (ExpX x) (ExpX x)       -- conditional expression
-  deriving (Eq, Ord, Show, Data, Typeable)
+--   deriving (Eq, Ord, Show, Data, Typeable)
 
 type family XVar x
 type family XCon x
