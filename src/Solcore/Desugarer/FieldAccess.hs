940c707af215113b001a91831c737fe012995fc1
Marcin Benke <marcin@benke.org>
2025-11-19 13:18:10 +0100

    FieldAccess: skeleton

--- src/Solcore/Desugarer/FieldAccess.hs
+++ src/Solcore/Desugarer/FieldAccess.hs
@@ -1,10 +1,120 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 module Solcore.Desugarer.FieldAccess where
 
-import Control.Monad.Identity
+import Data.List (mapAccumL, foldl')
+-- import Data.Maybe (mapMaybe)
+-- import Data.Set qualified as Set
+-- import Data.Set (Set)
+
+import GHC.Stack
+
+import Text.Pretty.Simple
 
 import Solcore.Frontend.Pretty.SolcorePretty
 import Solcore.Frontend.Syntax
 import Solcore.Primitives.Primitives
 
+type ContractName = Name
+type NmContract = Contract Name
+type NmField = Field Name
+type NmTopDecl = TopDecl Name
+type NmContractDecl = ContractDecl Name
+type NmBody = Body Name
+type NmStmt = Stmt Name
+
 fieldDesugarer :: CompUnit Name -> CompUnit Name
-fieldDesugarer cu = cu
+fieldDesugarer (CompUnit ims topdecls) = CompUnit ims (extras <> topdecls')
+  where
+    (extras, topdecls') = mapAccumL go mempty topdecls
+    go acc (TContr c) = (acc <> extraTopDeclsForContract c, TContr (transContract c))
+    go acc v = (acc, v)
+
+--------------------------------
+-- # Extra Top Decls
+--------------------------------
+
+extraTopDeclsForContract :: NmContract -> [NmTopDecl]
+extraTopDeclsForContract (Contract cname _ts cdecls) = do
+    let singName = singletonNameForContract cname
+    let contractSingDecl = TDataDef $ DataTy singName [] [Constr singName []]
+
+    let fields = getFields cdecls
+    let (_fieldTypes, extraFieldDecls) = foldl' (flip contractFieldStep) ([], []) fields
+    (contractSingDecl:extraFieldDecls)
+    where
+      -- given a list of contract field types so far and topdecls for them, amends them with data for another field
+      -- the types of previous fields are needed to construct field offset
+      contractFieldStep :: NmField -> ([Ty], [NmTopDecl]) -> ([Ty], [NmTopDecl])
+      contractFieldStep field (tys, topdecls) = (tys', topdecls') where
+          tys' = tys ++ [fieldTy field]
+          topdecls' = topdecls ++ extraTopDeclsForContractField cname field offset
+          offset = foldr pair unit tys
+
+
+extraTopDeclsForContractField :: ContractName -> NmField -> Ty -> [NmTopDecl]
+extraTopDeclsForContractField cname (Field fname fty _minit) offset = [selDecl, TInstDef sfInstance] where
+  -- data b_sel = n_sel
+  selName = selectorNameForField cname fname
+  selDecl = TDataDef $ DataTy selName [] [Constr selName []]
+  selType = TyCon selName []
+  -- instance StructField(ContractStorage(CCtx), fld1_sel):StructField(uint, ()) {}
+  ctxTy = TyCon "ContractStorage" [singletonTypeForContract cname]
+  sfInstance = Instance
+               { instDefault = False
+               , instVars = []
+               , instContext = []
+               , instName = "StructField"
+               , paramsTy = [fty, offset]
+               , mainTy = TyCon "StructField" [ctxTy, selType]
+               , instFunctions = []
+               }
+
+--------------------------------
+-- # Contract Desugaring
+--------------------------------
+
+transContract :: NmContract -> NmContract
+transContract c = c { decls = map (transCDecl fields) c.decls } where
+    fields = getFields c.decls
+
+transCDecl :: [NmField] -> NmContractDecl -> NmContractDecl
+transCDecl fields (CFunDecl fd) = CFunDecl $ fd { funDefBody = transBody fields fd.funDefBody }
+transCDecl _ d = d
+
+transBody :: [NmField] -> NmBody -> NmBody
+transBody fields = snd . mapAccumL (transStmt fields) mempty
+
+transStmt :: [NmField] -> [Name] -> NmStmt -> ([Name], NmStmt)
+transStmt _fields locals stmt = (locals, stmt)
+
+--------------------------------
+-- # Helpers
+--------------------------------
+
+getFields :: [NmContractDecl] -> [NmField]
+getFields cdecls = concatMap getF cdecls where
+    getF (CFieldDecl f) = [f]
+    getF _ = []
+
+appendToName :: Name -> String -> Name
+appendToName (Name s) t = Name (s <> t)
+appendToName (QualName n s) t = QualName n (s <> t)
+
+selectorNameForField :: Name -> Name -> Name
+selectorNameForField cname (Name fld) = Name(show cname <> fld <> "_sel")
+selectorNameForField _ n = notImplementedS "selectorNameForField" n
+
+singletonNameForContract :: Name -> Name
+singletonNameForContract cname = appendToName cname "Cxt"
+
+singletonTypeForContract :: Name -> Ty
+singletonTypeForContract cname = TyCon (singletonNameForContract cname) []
+
+singletonValForContract :: Name -> Exp Name
+singletonValForContract cname = Con (singletonNameForContract cname) []
+
+notImplemented :: (HasCallStack, Pretty a) => String -> a -> b
+notImplemented funName a = error $ concat [funName, " not implemented yet for ", pretty a]
+
+notImplementedS :: (HasCallStack, Show a) => String -> a -> b
+notImplementedS funName a = error $ concat [funName, " not implemented yet for ", show(pShow a)]
