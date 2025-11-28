abd2070b30fbbe041da4cf9e465a06cba646942e
Marcin Benke <marcin@benke.org>
2025-11-28 13:18:06 +0100

    Contract testing using testrunner and evmone

--- testsol.sh
+++ testsol.sh
@@ -10,21 +10,21 @@ function runsol() {
     file=$1
     echo $file
     shift
-    rm -f -v output1.hull Output.sol
+    rm -f -v output1.core Output.sol
     /usr/bin/time -f "Compilation time: %E" cabal run sol-core -- -f $file $* && \
-	cabal exec yule -- output1.hull -w -O --nodeploy > /dev/null && \
+	cabal exec yule -- output1.core -w -O --nodeploy > /dev/null && \
         forge script --via-ir Output.sol | egrep '(Gas|RESULT)'
 }
 
 
-function runhull() {
+function runcore() {
     echo $1
     rm -f -v Output.sol
     cabal exec yule -- $1 -w --nodeploy -O && forge script --via-ir Output.sol | egrep '(Gas|RESULT)'
 }
 
-function hevmhull() {
-     local base=$(basename $1 .hull)
+function hevmcore() {
+     local base=$(basename $1 .core)
      local yulfile=$base.yul
      echo $yulfile
      local hexfile=$base.hex
@@ -40,38 +40,56 @@ function hevmsol() {
     file=$1
     echo $file
     local base=$(basename $1 .solc)
-    local hull=output1.hull
+    local core=output1.core
     local hexfile=$base.hex
     local yulfile=$base.yul
     echo Hex: $hexfile
     shift
     cabal exec sol-core -- -f $file $* && \
-	cabal exec yule -- $hull --nodeploy -O -o $yulfile && \
+	cabal exec yule -- $core --nodeploy -O -o $yulfile && \
         solc --strict-assembly --bin --optimize $yulfile | tail -1 > $hexfile && \
         hevm exec --code $(cat $hexfile) | awk -f parse_hevm_output.awk
 
 }
 
-function deploysol() {
+function hevmsol() {
+
+    echo $*
+    file=$1
+    echo $file
+    local base=$(basename $1 .solc)
+    local core=output1.core
+    local hexfile=$base.hex
+    local yulfile=$base.yul
+    echo Hex: $hexfile
+    shift
+    cabal exec sol-core -- -f $file $* && \
+	cabal exec yule -- $core --nodeploy -O -o $yulfile && \
+        solc --strict-assembly --bin --optimize $yulfile | tail -1 > $hexfile && \
+        hevm exec --code $(cat $hexfile) | awk -f parse_hevm_output.awk
+
+}
+
+function solchex() {
     local file=$1
     shift
     echo "Solc: $file"
     local base=$(basename $file .solc)
-    local hull=output1.hull
-    echo "Hull: $hull"
+    local core=output1.core
+    echo "Hull: $core"
     local yulfile=$base.yul
     echo "Yul:  $yulfile"
     rm -f -v $yulfile
     cabal exec sol-core -- -f $file $* && \
-    cabal exec yule -- $hull -o $yulfile
+    cabal exec yule -- $core --compress -o $yulfile
     hex=$(solc --strict-assembly --bin --optimize --optimize-yul $yulfile | tail -1)
-    rawtx=$(cast mktx --private-key=$DEPLOYER_KEY --create $hex)
-    addr=$(cast publish $rawtx | jq .contractAddress | tr -d '"')
-    echo $addr
+    local hexfile=$base.hex
+    echo "Writing hex:  $hexfile"
+    echo $hex > $hexfile
 }
 
-function deployhull() {
-    local base=$(basename $1 .hull)
+function deploycore() {
+    local base=$(basename $1 .core)
     local yulfile=$base.yul
     echo $yulfile
     local hexfile=$base.hex
@@ -83,6 +101,28 @@ function deployhull() {
     echo $addr
 }
 
+# function deployyul() {
+#     local yulfile=$1
+#     local base=$(basename $1 .yul)
+#     hex=$(solc --strict-assembly --bin --optimize --optimize-yul $yulfile | tail -1)
+#     rawtx=$(cast mktx --private-key=$DEPLOYER_KEY --create $hex)
+#     addr=$(cast publish $rawtx | jq .contractAddress | tr -d '"')
+#     echo $addr
+# }
+
+function deployhull() {
+    local hull=$1
+    local base=$(basename $1 .core)
+    shift
+    local yulfile=$base.yul
+    echo $yulfile
+    local hexfile=$base.hex
+    rm -f -v $yulfile #$hexfile
+    echo cabal exec yule -- $hull -o $yulfile
+    cabal exec yule -- $hull -o $yulfile
+    deployyul $yulfile $*
+}
+
 function deployyul() {
     local yulfile=$1
     shift
@@ -91,6 +131,22 @@ function deployyul() {
     echo "Args: $*"
     echo "ABI-enc: $data"
     prog=$(solc --strict-assembly --bin --optimize --optimize-yul $yulfile | tail -1)
+    hex="$prog$data"
+    echo Hex: $hex
+    rawtx=$(cast mktx --private-key=$DEPLOYER_KEY --create $hex $*)
+    txoutput=$(cast publish $rawtx)
+    echo $txoutput | jq .
+    export contractAddress=$(echo $txoutput | jq .contractAddress | tr -d '"')
+    echo $contractAddress
+}
+
+function deployhex() {
+    local hexfile=$1
+    shift
+    local data=$(cast ae $* | cut -c 3-)
+    echo "Args: $*"
+    echo "ABI-enc: $data"
+    prog=$(cat $hexfile)
     hex="$prog$data"
     echo Hex: $hex
     rawtx=$(cast mktx --private-key=$DEPLOYER_KEY --create $hex $*)
@@ -116,7 +172,7 @@ function deployyul1() {
 }
 
 function hull() {
-     local base=$(basename $1 .hull)
+     local base=$(basename $1 .core)
      local yulfile=$base.yul
      rm -f -v $yulfile
      cabal exec yule -- $1 -o $yulfile
