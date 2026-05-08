5a94ad372bdaf86191c0f1c57aebe048faf5d9bf
Marcin Benke <marcin.benke@argot.org>
2026-05-08 19:36:46 +0200

    Hull/TypeCheck: add SFor

--- src/Language/Hull/TypeCheck.hs
+++ src/Language/Hull/TypeCheck.hs
@@ -76,6 +76,18 @@ checkStmt (SExpr e) =
   checkExpr e >> pure ()
 checkStmt (SAssembly stmts) =
   withLocalEnv (checkAsmBlock stmts)
+checkStmt (SFor initStmt cond post body) =
+  -- Variables declared in the init block are scoped over the entire for loop
+  -- (cond, post, body), matching Yul's for-loop scoping rules.
+  withLocalEnv $ do
+    checkBody (blockStmts initStmt)
+    te <- checkExpr cond
+    expectType TBool te
+    checkStmt post
+    checkStmt body
+  where
+    blockStmts (SBlock ss) = ss
+    blockStmts s = [s]
 checkStmt (SRevert _) = pure ()
 checkStmt (SComment _) = pure ()
 
