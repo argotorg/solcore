745ce70078515f4f49b620f9c5805cfcd8a34200
rodrigogribeiro <rodrigogribeiro@gmail.com>
2026-06-02 04:16:56 -0300

    Fixes empty blocks and remove ';' from Yul syntax

--- src/Solcore/Frontend/Pretty/TreePretty.hs
+++ src/Solcore/Frontend/Pretty/TreePretty.hs
@@ -306,6 +306,7 @@ instance Pretty Stmt where
       <+> lbrace
       $$ nest 3 (ppr body)
       $$ rbrace
+  ppr EmptyStmt = empty
 
 pprForClause :: Stmt -> Doc
 pprForClause (Assign n e) = ppr n <+> equals <+> ppr e
@@ -313,6 +314,7 @@ pprForClause (StmtPlusEq e1 e2) = hsep [ppr e1, text "+=", ppr e2]
 pprForClause (StmtMinusEq e1 e2) = hsep [ppr e1, text "-=", ppr e2]
 pprForClause (Let n ty m) = text "let" <+> ppr n <+> pprOptTy ty <+> pprForInitOpt m
 pprForClause (StmtExp e) = ppr e
+pprForClause EmptyStmt = empty
 pprForClause s = ppr s
 
 pprForInitOpt :: Maybe Exp -> Doc
